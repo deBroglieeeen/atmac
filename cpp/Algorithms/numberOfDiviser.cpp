78db58c78f6c83d24d85d805e4c08c8aef5850ad
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2021-02-06 12:29:03 +0900

    wrote output all divisors but you have to erase 0 and n and think more effective algorithm now order is O(n^2)

--- cpp/Algorithms/numberOfDiviser.cpp
+++ cpp/Algorithms/numberOfDiviser.cpp
@@ -1,9 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,n) for(int i=0; i < (n); ++i)
-using ll = Long Long;
+using ll = long long;
 using P = pair<int,int>;
 
-int main(){
+// O(n^2)
+vector<int> getDivisorsBrute(int n) {
+  vector<int> Divisors(n);
+  rep(i,n+1) {
+    if(i == 0) continue;
+    bool isChecked = false;
+    rep(j,n) {
+      if(Divisors[j] == i) isChecked = true;
+      break;
+    }
+    if(!isChecked && n % i == 0) Divisors.push_back(i);
+  }
+  return Divisors;
+}
 
+int main(){
+  vector<int> Div;
+  int n;
+  cin >> n;
+  Div = getDivisorsBrute(n);
+  rep(i,n) {
+    if(Div[i] == 0) Div.erase(Div.begin() + i);
+  }
+  rep(i,n) cout << Div[i] << endl;
 }
